5f1772cb24688d8ac22717aec6dcbcc80c15998e
GonzaloFernandez45 <gonzalo.fernandezeizaguirre@usp.ceu.es>
2025-11-05 16:14:09 +0100

    JDBCSymptoms Update

---
+++ src/main/java/jdbc/JDBCSymptomManager.java
@@ -0,0 +1,53 @@
+package jdbc;
+
+import interfaces.SymptomManager;
+import pojos.Symptom;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+public class JDBCSymptomManager implements SymptomManager {
+
+    private static Connection c;
+    private ConnectionManager conMan;
+    public JDBCSymptomManager(ConnectionManager conMan) {
+        this.conMan = conMan;
+        this.c = conMan.getConnection();
+    }
+
+    @Override
+    public void addSymptom(Symptom s) {
+
+    }
+
+    @Override
+    public List<Symptom> listSymptoms() {
+        return List.of();
+    }
+
+    @Override
+    public Symptom getSymptom(Symptom s) {
+        return null;
+    }
+
+    @Override
+    public Symptom getSymptomById(int symptomId) {
+        Symptom symptom = null;
+        String query = "SELECT * FROM symptoms WHERE id = ?";
+
+        try (PreparedStatement stmt = c.prepareStatement(query)) {
+            stmt.setInt(1, symptomId);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                symptom = new Symptom(rs.getInt("id"), rs.getString("name"));
+            }
+        }catch (SQLException e) {
+            System.err.println("Error fetching symptom by ID: " + e.getMessage());
+            e.printStackTrace();
+        }
+        return symptom;
+    }
+}
