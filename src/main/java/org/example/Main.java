6492bfa407519adf64056d1e2442013935a4e23c
Martadiezr <marta.diezrosa@usp.ceu.es>
2025-11-19 19:06:48 +0100

    prueba

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,6 +13,7 @@ import ReceiveData.*;
 import pojos.Patient;
 import pojos.Role;
 import pojos.User;
+//prueba
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
