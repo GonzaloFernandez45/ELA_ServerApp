e73b80a174f59dffde65ffacd8cb16598a4bafcf
FMedinaJ <fernando.demedinajunyent@usp.ceu.es>
2025-11-22 19:04:10 +0100

    Doctor Log in and register

--- src/main/java/ReceiveData/SendDataViaNetwork.java
+++ src/main/java/ReceiveData/SendDataViaNetwork.java
@@ -59,7 +59,7 @@ public class SendDataViaNetwork {
     }
 
     public void sendDoctor(Doctor doctor) throws IOException {
-        dataOutputStream.writeInt(doctor.getId());
+        //dataOutputStream.writeInt(doctor.getId());
         dataOutputStream.writeUTF(doctor.getName());
         dataOutputStream.writeUTF(doctor.getSurname());
         dataOutputStream.writeUTF(doctor.getDNI());
